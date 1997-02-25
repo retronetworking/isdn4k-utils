75a3fc0f7b9c8c968694ec3b0bf869fc11a1b226
Michael 'Ghandi' Herold <michael@abadonna.mayn.de>
1997-02-25 17:41:09 +0000

    Added vbox to cvs tree

---
+++ vbox/src/rcgetty.h
@@ -0,0 +1,14 @@
+/*
+** $Id$
+**
+** Copyright (C) 1996, 1997 Michael 'Ghandi' Herold
+*/
+
+#ifndef _VBOX_RCGETTY_H
+#define _VBOX_RCGETTY_H 1
+
+/** Prototypes ***********************************************************/
+
+extern int getty_get_settings(char *);
+
+#endif /* _VBOX_RCGETTY_H */
