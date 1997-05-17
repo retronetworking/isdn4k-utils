944c2e9c9d74803f5ccd26b9fafdf72e9ee06e6a
Fritz Elfert <fritz@wuemaus.franken.de>
1997-05-17 12:23:41 +0000

    Corrected some Copyright notes to refer to GPL.

--- imon/imon.c
+++ imon/imon.c
@@ -4,7 +4,25 @@
  * original iMON source (c) Michael Knigge
  * heavily modified and extended by Fritz Elfert
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  * $Log$
+ * Revision 1.2  1997/03/20 00:19:01  luethje
+ * inserted the line #include <errno.h> in avmb1/avmcapictrl.c and imon/imon.c,
+ * some bugfixes, new structure in isdnlog/isdnrep/isdnrep.c.
+ *
  * Revision 1.1  1997/02/17 00:09:03  fritz
  * New CVS tree
  *
