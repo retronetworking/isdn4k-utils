a692c7e7dcafe778bbe5a363f759fe27d28f4ce0
Michael 'Ghandi' Herold <michael@abadonna.mayn.de>
1998-06-10 16:03:15 +0000

    - Some special string functions added.

---
+++ vbox3/src/stringutils.h
@@ -0,0 +1,15 @@
+/*
+** $Id$
+**
+** Copyright 1997-1998 by Michael 'Ghandi' Herold
+*/
+
+#ifndef _VBOX_STRINGUTILS_H
+#define _VBOX_STRINGUTILS_H 1
+
+/** Prototypes ***********************************************************/
+
+extern void xstrncpy(char *, char *, int);
+extern long xstrtol(char *, long);
+
+#endif /* _VBOX_STRINGUTILS_H */
