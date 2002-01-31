487443b0206b0343abd12cb797ed3c2f6bea7848
Paul Slootman <paul@wurtel.demon.nl>
2002-01-31 18:59:24 +0000

    Supply defaults for ISDN_MAX_CHANNELS and ISDN_MAX_DRIVERS if not defined.

--- imontty/imontty.h
+++ imontty/imontty.h
@@ -5,5 +5,12 @@
  * $Id$
  */
 
+#ifndef ISDN_MAX_CHANNELS
+# define ISDN_MAX_CHANNELS      64       /* largest common denominator */
+#endif
+#ifndef ISDN_MAX_DRIVERS
+# define ISDN_MAX_DRIVERS       32       /* ditto */
+#endif
+
 #define		IM_BUFSIZE	10 + (22 * ISDN_MAX_CHANNELS)
 #define		IM_VERSION	"0.5"
