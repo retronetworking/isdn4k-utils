73a4696de009fe46e49d1cd9bd50498cfde1594c
cvs2git <i4ldeveloper@lists.isdn4linux.de>
2006-05-17 19:54:00 +0000

    This commit was manufactured by cvs2git to create tag 'v0_1_12'.

--- isdnlog/tools/zone/upack.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UPACK_H_
-#define _UPACK_H_
-
-void tools_pack32(unsigned char *buf, UL num);
-UL tools_unpack32(unsigned char *buf);
-void tools_pack16(unsigned char *buf, US num);
-US tools_unpack16(unsigned char *buf);
-
-#endif
